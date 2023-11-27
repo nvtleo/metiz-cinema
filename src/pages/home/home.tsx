5569af8c86828fa9963163d756d9fc429f576622
Trung <trungnguyen.02082000@gmail.com>
2023-11-27 11:37:25 +0700

    b

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Slider } from './slider/slider'
 import ListCard from '../../components/list-card/list-card'
-import { axiosWthoutAuth } from '../../service';
+// import { axiosWthoutAuth } from '../../service';
 import { useState, useEffect } from "react";
 import { getAllMovie } from '../../service/product.service';
 import { IIFE } from '../../util';
-import { TMovie, convertMovie } from './convert';
+import { convertMovie } from './convert';
 import { Tcard } from '../../components/list-card/card/card';
 import { useScrollToTop } from '../../hook/rool-to-top';
 
@@ -14,7 +14,7 @@ function Home() {
     const [listMovie, setListMovie] = useState<Tcard[]>([]);
     useEffect(() => {
         IIFE(async () => {
-            const resp = await getAllMovie();
+            const resp: any = await getAllMovie();
             setListMovie(convertMovie(resp))
         })
     }, [])
